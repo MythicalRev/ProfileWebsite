ff09bb6ff360ea6349bee55c817f2935c5e7cd86
MythicalRev <landonsciara@gmail.com>
2024-12-17 22:18:26 -0500

    firstUpload

---
+++ index.js
@@ -0,0 +1,18 @@
+// Values
+const express = require("express");
+const bodyParser = require('body-parser');
+const path = require('path');
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+  res.render('home');
+});
+
+app.listen(3000, () => {
+  console.log("Project is ready!");
+});
