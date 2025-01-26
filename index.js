494626843e532323d4371ae8ae0c84cb68728283
MythicalRev <landonsciara@gmail.com>
2025-01-26 16:11:38 -0500

    complete recreation cuz fuck it

--- index.js
+++ index.js
@@ -10,9 +10,16 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
-  res.render('home');
+
+  fetch('https://api.lanyard.rest/v1/users/501514913026408448', { 
+    method: 'GET'
+  })
+  .then(function(response) { return response.json(); })
+  .then(function(json) {
+    res.render('home', { json: json });
+  });
 });
 
-app.listen(3000, () => {
+app.listen(8000, () => {
   console.log("Project is ready!");
 });
